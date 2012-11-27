009be8a96adadea7f5c956c0ecbb8858db0057e1
Maisa Engineering <contact@maisasolutions.com>
2012-11-27 15:51:09 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GpsPositioning::Application.config.secret_token = '51827c056c568b904177a61ef8ae856b6a13594c0b7040ebb04b9613a56a61294d88c90b6bf8c994e3c421e2c7b907e435abbce28a24e06524f9c7ad23315f38'
